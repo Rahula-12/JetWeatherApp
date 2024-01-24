c9c98d714011fe1aec657076c4ba87ff2c9838e4
rahul <ra9853044@gmail.com>
2024-01-24 20:17:28 +0530

    setting up app navigation

---
+++ app/src/main/java/com/example/jetweatherforecast/screens/WeatherSplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.jetweatherforecast.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun WeatherSplashScreen(
+    navController: NavController
+) {
+
+}
