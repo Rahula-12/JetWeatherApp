5c8ff638516d8c44a71715abd8fbb090a71d0e7a
rahul <ra9853044@gmail.com>
2024-01-26 16:40:37 +0530

    setting up model classes

--- app/src/main/java/com/example/jetweatherforecast/navigation/WeatherNavigation.kt
+++ app/src/main/java/com/example/jetweatherforecast/navigation/WeatherNavigation.kt
@@ -4,8 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.jetweatherforecast.screens.WeatherMainScreen
-import com.example.jetweatherforecast.screens.WeatherSplashScreen
+import com.example.jetweatherforecast.screens.main.WeatherMainScreen
+import com.example.jetweatherforecast.screens.splash.WeatherSplashScreen
 
 @Composable
 fun WeatherNavigation() {
