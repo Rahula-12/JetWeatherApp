31598b205a577b109c685e3c398a49f906520b03
rahul <ra9853044@gmail.com>
2024-01-26 16:40:37 +0530

    setting up model classes

--- app/src/main/java/com/example/jetweatherforecast/screens/main/WeatherMainScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/main/WeatherMainScreen.kt
@@ -1,4 +1,4 @@
-package com.example.jetweatherforecast.screens
+package com.example.jetweatherforecast.screens.main
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
