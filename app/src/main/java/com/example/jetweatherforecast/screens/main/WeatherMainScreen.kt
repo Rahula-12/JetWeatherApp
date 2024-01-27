dfacf630c9d265d1a16fe1d635901364f8793ab0
rahul <ra9853044@gmail.com>
2024-01-28 00:39:54 +0530

    setting up the appbar

--- app/src/main/java/com/example/jetweatherforecast/screens/main/WeatherMainScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/main/WeatherMainScreen.kt
@@ -1,29 +1,54 @@
 package com.example.jetweatherforecast.screens.main
 
+import android.util.Log
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.jetweatherforecast.data.DataOrException
 import com.example.jetweatherforecast.model.Weather
+import com.example.jetweatherforecast.widgets.WeatherAppBar
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun WeatherMainScreen(
     navController: NavController,
     mainViewModel: MainViewModel= hiltViewModel()
 ){
-    ShowData(mainViewModel)
+    val weather= produceState(initialValue = DataOrException<Weather,Boolean,Exception>(), producer = {
+        value=mainViewModel.getWeather("Amritsar")
+    }).value
+    if(weather.loading == true){
+        CircularProgressIndicator()
+    }
+    else if(weather.data!=null){
+        MainScaffold(weather.data!!)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun MainScaffold(weather:Weather) {
+    Scaffold(
+        topBar = {
+            WeatherAppBar(title = weather.city.name)
+        }
+    ) {
+        MainContent(data=weather,modifier=Modifier.padding(it))
+    }
 }
 
 @Composable
-private fun ShowData(mainViewModel: MainViewModel) {
-    val weather= mainViewModel.dataOrException
-    if(weather.value.loading == true){
-            CircularProgressIndicator()
-    }
-    else{
-        Text(text = "MainScreen ${mainViewModel.dataOrException.value.data!!.city}")
-    }
+fun MainContent(data: Weather,modifier: Modifier=Modifier) {
+    Text(text = data.city.name)
 }
