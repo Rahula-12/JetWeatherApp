c6a31fec7c820787edd6175460c0887fb4229b23
rahul <ra9853044@gmail.com>
2024-02-01 16:01:50 +0530

    creating the main screen-finalizing the top circle and data

--- app/src/main/java/com/example/jetweatherforecast/data/DataOrException.kt
+++ app/src/main/java/com/example/jetweatherforecast/data/DataOrException.kt
@@ -2,6 +2,6 @@ package com.example.jetweatherforecast.data
 
 data class DataOrException<T,Boolean,E:Exception>(
     var data:T?=null,
-    var loading: kotlin.Boolean?=null,
+    var loading: kotlin.Boolean?=true,
     var exception: E?=null
 )
