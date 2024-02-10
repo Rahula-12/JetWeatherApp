7ebf2f5b8bec12657abc9963c8c4fc92b4876839
rahul <ra9853044@gmail.com>
2024-02-10 20:57:42 +0530

    added humidity,wind and pressure row

--- app/src/main/java/com/example/jetweatherforecast/screens/main/WeatherMainScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/main/WeatherMainScreen.kt
@@ -4,12 +4,15 @@ import android.util.Log
 import android.widget.ImageView
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -21,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,6 +33,7 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.bumptech.glide.Glide
+import com.example.jetweatherforecast.R
 import com.example.jetweatherforecast.data.DataOrException
 import com.example.jetweatherforecast.model.Weather
 import com.example.jetweatherforecast.utils.formatDate
@@ -60,7 +65,9 @@ private fun MainScaffold(weather:Weather,navController: NavController) {
         }
 
     ) {
-        MainContent(data=weather,modifier=Modifier.padding(it))
+        MainContent(data=weather,
+            //modifier = Modifier.padding(it)
+        )
     }
 }
 
@@ -70,24 +77,25 @@ fun MainContent(data: Weather,modifier: Modifier=Modifier) {
     Log.d("url",weatherImageUrl)
     Column(
         modifier = modifier
-            .padding(4.dp)
-            .fillMaxWidth(),
+            .fillMaxWidth().padding(top = 60.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
             text= formatDate(data.list[0].dt),
             style=MaterialTheme.typography.titleSmall,
-            color=MaterialTheme.colorScheme.onSecondary,
+            color=Color.Black,
             fontWeight = FontWeight.SemiBold,
-            modifier = modifier.padding(2.dp)
+            modifier = modifier.padding(
+                top=20.dp,
+                bottom = 10.dp
+            )
         )
         Surface(
             color = Color(0xFFFFC400),
             shape = CircleShape,
             modifier = modifier
                 .size(220.dp)
-                .padding(2.dp)
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -97,6 +105,40 @@ fun MainContent(data: Weather,modifier: Modifier=Modifier) {
                 Text(text= formatDecimals(data.list[0].temp.day)+"°", style = MaterialTheme.typography.headlineMedium, fontWeight = FontWeight.Bold, color = Color.Black)
                 Text(text=data.list[0].weather[0].description, style = MaterialTheme.typography.headlineMedium, fontStyle = FontStyle.Italic)
             }
+        }
+        HumidityWindPressureRow(data)
+        Divider()
+    }
+}
+
+@Composable
+private fun HumidityWindPressureRow(data: Weather) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.humidity),
+                contentDescription = "humidity icon", modifier = Modifier.size(20.dp)
+            )
+            Text(text = " ${data.list[0].humidity}%", style = MaterialTheme.typography.bodySmall)
+        }
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.pressure),
+                contentDescription = "pressure icon", modifier = Modifier.size(20.dp)
+            )
+            Text(text = " ${data.list[0].pressure} psi", style = MaterialTheme.typography.bodySmall)
+        }
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.wind),
+                contentDescription = "wind icon", modifier = Modifier.size(20.dp)
+            )
+            Text(text = " ${data.list[0].speed} mph", style = MaterialTheme.typography.bodySmall)
         }
     }
 }
@@ -121,7 +163,7 @@ private fun WeatherImage(weatherImageUrl: String="https://openweathermap.org/img
 //    ) {
         AndroidView(
             factory = {imageView},
-            modifier = Modifier.size(100.dp)
+            modifier = Modifier.size(80.dp)
         )
 //    }
 }
