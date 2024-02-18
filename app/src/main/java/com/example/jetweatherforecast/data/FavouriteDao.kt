86497a189e55f9b154772e034854a1eed98db3ae
rahul <ra9853044@gmail.com>
2024-02-18 17:57:44 +0530

    created settingsviewmodel

--- app/src/main/java/com/example/jetweatherforecast/data/FavouriteDao.kt
+++ app/src/main/java/com/example/jetweatherforecast/data/FavouriteDao.kt
@@ -6,6 +6,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.jetweatherforecast.model.Favourite
+import com.example.jetweatherforecast.model.Measurement
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -25,4 +26,17 @@ interface FavouriteDao {
 
     @Query("Select * from fav_tbl where city=:city")
     suspend fun getFavouriteByCity(city:String):Favourite
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMeasurement(measurement: Measurement)
+
+    @Delete
+    suspend fun deleteMeasurement(measurement: Measurement)
+
+    @Query("Select * from measurement_tbl")
+    fun getAllMeasurements(): Flow<List<Measurement>>
+
+    @Query("Delete from measurement_tbl")
+    suspend fun deleteAllMeasurements()
+
 }
